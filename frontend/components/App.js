624890a2601d22adc5c4d978f53ff47d2434d421
Steven Fehr <shfehr@gmail.com>
2023-09-29 09:59:40 -0500

    That toggle switch got me in the end!!!

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,52 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  
+  const [characters, setCharacters] = useState([])
+
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    axios.get(urlPeople)
+      .then(res => {
+      setCharacters(res.data)
+      }).catch(err => console.error(err))
+   }, [])
+
+   useEffect(() => {
+    axios.get(urlPlanets)
+      .then(res => {
+      setPlanets(res.data)
+      }).catch(err => console.error(err))
+   }, [])
+   
   // ❗ Create effects to fetch the data and put it in state
+
+  for (let i = 0; i < planets.length; i++) {
+    const planetID = planets[i].id
+    const planetName = planets[i].name
+    for (let i = 0; i < characters.length; i++) {
+     if (characters[i].homeworld === planetID) {
+        characters[i].homeworld = planetName
+        // console.log(`character ID ${characters[i].homeworld} == ${planetID}`)
+     }
+     
+    }
+ }
+ console.log(characters)
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      { characters.map(char => {
+        return <Character name = {char.name} homeworld = {char.homeworld} key={char.id}/>
+      })
+      /* <div class="character-card">
+        <h3 class='character-name'>{character.name}</h3>
+      </div> */}
+      {/*
+      ❗ Map over the data in state, rendering a Character at each iteration */}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { __PRIVATE__ } from 'styled-components'
 
-function Character() { // ❗ Add the props
+
+function Character(props) { 
+
+const { name, homeworld } = props
+  console.log(homeworld)
+  const[toggle, setToggle] = useState(false)
+  // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+ 
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+     <div onClick = {() => setToggle(!toggle)} className ='character-card'>
+        <h3 className = 'character-name'>{name}</h3>
+        { toggle && 
+          <p>Planet: 
+          <span className='character-planet'>{homeworld}</span> 
+          </p>   
+        }
+    
+        
+      </div> 
   )
 }
 
